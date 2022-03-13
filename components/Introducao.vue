e55561a309a8435e85066fc24896ddd2a534b8b2
Fupi <98890458+GabZanMacaw@users.noreply.github.com>
2022-03-13 18:13:26 -0300

    fjifdsisdf

---
+++ components/Introducao.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>OLA</h1>
+    <p>{{ texto }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["texto"],
+};
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+p {
+  color: orange;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Introducao :texto="contagem" />
+    <button @click="adicionarUm()">adicionar um</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contagem: 0,
+    };
+  },
+  methods: {
+    adicionarUm() {
+      this.contagem += 1;
+    },
+  },
+};
+</script>
+
+<style></style>
